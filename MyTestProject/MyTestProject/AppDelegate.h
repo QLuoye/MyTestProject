78d42325c75719837e4586a11360bfc85dff2855
TTT <float.luoye@gmail.com>
2017-05-02 10:22:14 +0800

    test commit

---
+++ MyTestProject/MyTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTestProject
+//
+//  Created by TTT on 2017/5/2.
+//  Copyright © 2017年 TTT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
